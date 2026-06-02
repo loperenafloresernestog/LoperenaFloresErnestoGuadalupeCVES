20240fb662249205433725a522c919bd033d48ea
Loperena Flores Ernesto Guadalupe <ernesto.loperena.f@gmail.com>
2026-06-01 19:39:27 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -34,16 +24,12 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 document.getElementById('btn-email').addEventListener('click', function() {
-    // 1. Obtenemos el correo desde el atributo data-email
     const email = this.getAttribute('data-email');
     
-    // 2. Usamos la API del navegador para copiar al portapapeles
     navigator.clipboard.writeText(email).then(() => {
-        // 3. Si se copia con éxito, mostramos la notificación
         const toast = document.getElementById('toast-notification');
         toast.classList.add('show');
         
-        // 4. Ocultamos la notificación después de 3 segundos (3000 milisegundos)
         setTimeout(() => {
             toast.classList.remove('show');
         }, 3000);
